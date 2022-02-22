495b013750e41679c9d1ee083735d0f84ee0d94a
TimeTravelerFromNow <sebastian.detering@gmail.com>
2022-02-21 20:45:33 -0800

    counter changed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     counter = Counter.find_or_create_by(name: "default")
-    counter.value = (counter.value || 0) + 1
+    counter.value = (counter.value || 0) + 10
     counter.save!
     render plain: "Counter #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
   end
