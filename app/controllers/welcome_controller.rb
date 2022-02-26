b075c6b3ce3f928a9a6654685c22057dd269f2f2
TimeTravelerFromNow <sebastian.detering@gmail.com>
2022-02-26 14:02:29 -0800

    removed welcome controller

--- app/controllers/welcome_controller.rb
+++
@@ -1,8 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    counter = Counter.find_or_create_by(name: "default")
-    counter.value = (counter.value || 0) + 10
-    counter.save!
-    render plain: "Counter #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
-  end
-end
