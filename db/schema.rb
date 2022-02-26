abcbeea9aa8664cf1e3657b8e8890ec5c1468467
TimeTravelerFromNow <sebastian.detering@gmail.com>
2022-02-26 13:15:34 -0800

    Rails Rudimentary Blog with Authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_22_034923) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_26_203425) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "counters", force: :cascade do |t|
     t.string "name"
     t.integer "value"
@@ -18,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_22_034923) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
