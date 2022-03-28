42a21f2827d621f95303c1c407cbacb313e43873
TimeTravelerFromNow <sebastian.detering@gmail.com>
2022-03-28 11:41:31 -0700

    favicon to Gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,6 +76,7 @@ task :deploy do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
+    invoke :'rails:assets_clean'
     invoke :'rails:assets_precompile'
     on :launch do
       command "sudo systemctl restart #{fetch(:user)}"
